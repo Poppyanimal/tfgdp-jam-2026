ce8c4a01d9027c49ac8ee03a532926fc38d36e38
Aubrey <Poppyanimal@Yahoo.com>
2026-08-04 15:12:01 -0500

    model updates + choppy movement fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,6 @@ using System;
 using Unity.Cinemachine;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
