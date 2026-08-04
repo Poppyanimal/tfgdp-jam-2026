e91c933ee0cd9f96e937dfd078f0dee96d369510
AnDrew <repooc.a@gmail.com>
2026-08-03 20:35:53 -0500

    cleanup begins

--- Assets/Scripts/Library/SharedLib.cs
+++ Assets/Scripts/Library/SharedLib.cs
@@ -57,4 +57,64 @@ public class SharedLib
     //Takes an angl(radians) and returns a vector of length 1 at that angle on the XZ plane)
     public static Vector3 angleToVector(float angle) { return new Vector3(Mathf.Cos(angle), 0f, Mathf.Sin(angle)).normalized; }
 
+
+    //Takes a vector and returns that vector's grade in degrees (that is, the angle Y forms with the XZ plane.)
+    public static float vectorToGrade(Vector3 normal) {
+        float xz = Mathf.Sqrt((normal.x * normal.x) + (normal.z * normal.z));
+        float beta_rad = Mathf.Atan(normal.y / xz);
+        float alpha_deg = 90 - Mathf.Rad2Deg * beta_rad;
+        return alpha_deg;
+    }
+
+
+
+
+
+
+    //takes an origin, a direction, and optionally a distance, layermask, and color.
+    public static RaycastHit castInDirection(Vector3 origin, Vector3 direction                                                                           ) { return castInDirection( origin, direction, direction.magnitude , "Default"              ) ;}
+    public static RaycastHit castInDirection(Vector3 origin, Vector3 direction,                                          Color castColor = default(Color)) { return castInDirection( origin, direction, direction.magnitude , "Default", castColor   ) ;}
+    public static RaycastHit castInDirection(Vector3 origin, Vector3 direction, float dist,                              Color castColor = default(Color)) { return castInDirection( origin, direction, dist                , "Default", castColor   ) ;}
+    public static RaycastHit castInDirection(Vector3 origin, Vector3 direction, float dist, string layerMask="Default",  Color castColor = default(Color)) {
+        RaycastHit hit;
+        Ray r = new Ray(origin, direction);
+        bool success = Physics.Raycast(r, out hit, dist, LayerMask.GetMask(layerMask), QueryTriggerInteraction.UseGlobal);
+
+        if (!castColor.Equals(default(Color))) Debug.DrawRay( origin, direction * (success?hit.distance:dist), castColor, .5f);
+
+        return hit;
+    }
+
+    //Takes an origin, and facing angle, a fov angle, a distance and returns a raycastHit[3] of raycasts windershins to, at, and clockwise off the facing angle (seporated by the FOV
+    public static RaycastHit[] castWFC(Vector3 origin, float facing, float fov, float dist, bool drawCast) { return castWFC(origin, facing, fov,dist, "Default", drawCast); }
+    public static RaycastHit[] castWFC(Vector3 origin, float facing, float fov, float dist, string layerMask="Default", bool drawCast=false)
+    {        
+		float windershinsAngle = Mathf.Deg2Rad * (facing + fov);
+        float clockwiseAngle   = Mathf.Deg2Rad * (facing - fov);
+
+		Vector3 dir3windershins = angleToVector(windershinsAngle);
+        Vector3 dir3facing      = angleToVector(facing);
+		Vector3 dir3clockwise   = angleToVector(clockwiseAngle);
+        
+        RaycastHit[] hitWFC = new RaycastHit[3];
+        if (drawCast) {
+            hitWFC[0] = castInDirection(origin, dir3windershins  , dist, layerMask, Color.blue);
+		    hitWFC[1] = castInDirection(origin, dir3facing       , dist, layerMask, Color.cyan);
+		    hitWFC[2] = castInDirection(origin, dir3clockwise    , dist, layerMask, Color.green);
+        }
+        else {
+            hitWFC[0] = castInDirection(origin, dir3windershins  , dist, layerMask);
+		    hitWFC[1] = castInDirection(origin, dir3facing       , dist, layerMask);
+		    hitWFC[2] = castInDirection(origin, dir3clockwise    , dist, layerMask);
+        }
+
+        return hitWFC;
+    }
+
+
+
+
+
+
+
 }
