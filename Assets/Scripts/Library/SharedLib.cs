5c148a3e9402835395adbf7c468c87c18d809f09
AnDrew <repooc.a@gmail.com>
2026-08-04 04:52:20 -0500

    Fixed the movement system (I hope)
    Some changes to the SharedLib and Player Controller to reorganize everything and make it easier to read/modify.

--- Assets/Scripts/Library/SharedLib.cs
+++ Assets/Scripts/Library/SharedLib.cs
@@ -17,50 +17,48 @@ public class SharedLib
         return newIndex;
     }
 
-    //----------------------------- 2D rotation in 3D space (ignore z)
 
-    public static Vector2 rotateVector2(float angle, Vector2 startV) { return rotateVector2eul(angle, startV); }
-    public static Vector2 rotateVector2eul(float angle, Vector2 startV) { return rotateVector2rad(Mathf.Deg2Rad * angle, startV); }
+    #region Angle to Angle :: 1
 
-    //applies the rotation matrix ( [[cosA, -sinA], [sinA, cosA]] ) to the vector.
-    public static Vector2 rotateVector2rad(float angleR, Vector2 startV)
-    {
-        return new Vector2(
-            Mathf.Cos(angleR) * startV.x - Mathf.Sin(angleR) * startV.y,
-            Mathf.Sin(angleR) * startV.x + Mathf.Cos(angleR) * startV.y);
-    }
+    //Bounds a angle to the degrees of 0 <= x < 360
+    public static float angleToBoundedDegrees(float ee)  { return ((ee<0 ? 360f:0f) + ee) % 360f; }
 
+	#endregion
 
-    //Gives the angle of from <0,1> to [target-Origin]
-    public static float angleToTarget(Vector2 origin, Vector2 target)
-    {
-        return Mathf.Atan2(target.y - origin.y, target.x - origin.x) * Mathf.Rad2Deg + 90;
-    }
-
-    //
-
-    //Returns the Angle between two vectors.
-    public static float vectorToFlatAngle(Vector2 aa) { return angleBetweenVectors( aa, Vector2.right) ;}
-    public static float angleBetweenVectors(Vector2 aa, Vector2 bb) { return Mathf.Atan2(CrossProduct(aa,bb), DotProduct(aa,bb)) ; }
-    private static float DotProduct  (Vector2 aa, Vector2 bb) { return aa.x * bb.x + aa.y * bb.y; }
-    private static float CrossProduct(Vector2 aa, Vector2 bb) { return aa.x * bb.y - aa.y * bb.x; }
-
-
-    //Clamps a angle to the degree bounds of 0<=x<360
-    public static float simplifyEuler(float e)
-    {
-        if(e<0)
-            e += 360f;
-        e %= 360f;
-        return e;
-    }
-    
+    #region Angle to Vector :: 2 -1
     //Takes an angl(radians) and returns a vector of length 1 at that angle on the XZ plane)
-    public static Vector2 angleToVector2(float angle) { return new Vector3(Mathf.Sin(angle),     Mathf.Cos(angle)).normalized; }
-    public static Vector3 angleToVector3(float angle) { return new Vector3(Mathf.Sin(angle), 0f, Mathf.Cos(angle)).normalized; }
+    public static Vector3 angleToVector3     (float angle) { return new Vector3(Mathf.Sin(Mathf.Deg2Rad * angle), 0f, Mathf.Cos(Mathf.Deg2Rad * angle)).normalized; }
+    private static Vector3 angleToVector3_rad(float angle) { return new Vector3(Mathf.Sin(                angle), 0f, Mathf.Cos(                angle)).normalized; }
+
+    public static Vector3[] generateWFC(Vector3 origin, float facing, float fov) {
+        float windershinsAngle = facing + fov;
+        float clockwiseAngle = facing - fov;
+
+        Vector3[] dir3WFC = new Vector3[3];
+        dir3WFC[0] = angleToVector3_rad(Mathf.Deg2Rad * windershinsAngle);
+        dir3WFC[1] = angleToVector3_rad(Mathf.Deg2Rad * facing);
+        dir3WFC[2] = angleToVector3_rad(Mathf.Deg2Rad * clockwiseAngle);
+
+        return dir3WFC;
+
+    }
+
+	#endregion
 
 
-    //Takes a vector and returns that vector's grade in degrees (that is, the angle Y forms with the XZ plane.)
+
+    #region Vector to Angle :: 3 - 2
+
+    //takes 
+
+    
+    public static float vectorToAngle(Vector2 aa) { return angleBetweenVectors( aa, Vector2.right) ;}
+
+    public static float angleBetweenVectors(Vector2 aa            ) { return Mathf.Rad2Deg * Mathf.Atan2(crossProduct(aa,Vector2.right), dotProduct(aa,Vector2.right)) ; }
+    public static float angleBetweenVectors(Vector2 aa, Vector2 bb) { return Mathf.Rad2Deg * Mathf.Atan2(crossProduct(aa,bb           ), dotProduct(aa,bb           )) ; }
+    private static float dotProduct  (Vector2 aa, Vector2 bb) { return aa.x * bb.x + aa.y * bb.y; }
+    private static float crossProduct(Vector2 aa, Vector2 bb) { return aa.x * bb.y - aa.y * bb.x; }
+
     public static float vectorToGrade(Vector3 normal) {
         float xz = Mathf.Sqrt((normal.x * normal.x) + (normal.z * normal.z));
         float beta_rad = Mathf.Atan(normal.y / xz);
@@ -68,12 +66,59 @@ public class SharedLib
         return alpha_deg;
     }
 
+	#endregion
+
+    #region Vector to Vector :: 2 ~2
+
+    //Takes a vector and adds a third axis with a value of zero. Defaults to Y
+    public static Vector3 vector2to3(Vector2 v2) { return new Vector3(v2.x,0f,v2.y); }
+    public static Vector3 vector2to3(Vector2 v2, string axis) { 
+        switch (axis) {
+            case "x": case "X": return new Vector3  (0f, v2.x,v2.y);
+            case "z": case "Z": return    (Vector3) v2;
+            case "y": case "Y": return vector2to3(v2);
+            default: Debug.LogWarning("Unexpected String Value: defaulted to \"Y\""); return vector2to3(v2);
+        }
+         }
+
+
+    //Takes a vector and restricts it to its planar components by removing an axis. Defaults to XZ.
+    public static Vector2 vector3to2(Vector3 v3) { return new Vector2(v3.x,   v3.z); }
+    public static Vector2 vector3to2(Vector3 v3, string plane) {
+        switch (plane) {
+            case "xy": case "XY": return (Vector2) v3;
+            case "yz": case "YZ": return new Vector2(v3.y, v3.z);
+            case "xz": case "XZ": return vector3to2(v3);
+            default: Debug.LogWarning("Unexpected String Value: defaulted to \"XZ\""); return vector3to2(v3);
+        }
+    }
+
+
+	#endregion
 
 
 
+	#region Angle on Angle :: 0
 
+	#endregion
 
-    //takes an origin, a direction, and optionally a distance, layermask, and color.
+#region Angle on Vector :: 1 -1
+
+	//applies the rotation matrix ( [[cosA, -sinA], [sinA, cosA]] ) to the vector.
+	public static Vector2 rotateVector2(float angle, Vector2 startV) { return rotateVector2rad(Mathf.Deg2Rad * angle, startV); }
+    private static Vector2 rotateVector2rad(float angleR, Vector2 startV)
+    {
+        return new Vector2(
+            Mathf.Cos(angleR) * startV.x - Mathf.Sin(angleR) * startV.y,
+            Mathf.Sin(angleR) * startV.x + Mathf.Cos(angleR) * startV.y);
+    }
+
+	#endregion
+
+    
+    #region create RaycastHit :: 2 ~4
+
+    //takes an origin, a direction, and optionally a distance, layermask, and color. Returns a Raycasthit for those values. Draws the ray if given a color.
     public static RaycastHit castInDirection(Vector3 origin, Vector3 direction                                                                           ) { return castInDirection( origin, direction, direction.magnitude , "Default"              ) ;}
     public static RaycastHit castInDirection(Vector3 origin, Vector3 direction,                                          Color castColor = default(Color)) { return castInDirection( origin, direction, direction.magnitude , "Default", castColor   ) ;}
     public static RaycastHit castInDirection(Vector3 origin, Vector3 direction, float dist,                              Color castColor = default(Color)) { return castInDirection( origin, direction, dist                , "Default", castColor   ) ;}
@@ -87,7 +132,7 @@ public class SharedLib
         return hit;
     }
 
-    //Takes an origin, and facing angle, a fov angle, a distance and returns a raycastHit[3] of raycasts windershins to, at, and clockwise off the facing angle (seporated by the FOV
+    //Takes an origin, and facing angle, a fov angle, a distance. returns a RaycastHit[3] through generateWFC(...) and castInDirection(...). Draws all three of the Rays if passed a boolean.
     public static RaycastHit[] castWFC(Vector3 origin, float facing, float fov, float dist, bool drawCast) { return castWFC(origin, facing, fov, dist, "Default", drawCast); }
     public static RaycastHit[] castWFC(Vector3 origin, float facing, float fov, float dist, string layerMask="Default", bool drawCast=false)
     {        
@@ -108,22 +153,6 @@ public class SharedLib
         return hitWFC;
     }
 
-    public static Vector3[] generateWFC(Vector3 origin, float facing, float fov) {
-        float windershinsAngle = facing + fov;
-        float clockwiseAngle   = facing - fov;
-
-        Vector3[] dir3WFC= new Vector3[3];
-		dir3WFC[0] = angleToVector3(Mathf.Deg2Rad * windershinsAngle    );
-        dir3WFC[1] = angleToVector3(Mathf.Deg2Rad * facing              );
-		dir3WFC[2] = angleToVector3(Mathf.Deg2Rad * clockwiseAngle      );
-
-        return dir3WFC;
-
-    }
-
-
-
-
-
+	#endregion
 
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -36,8 +36,8 @@ public class PlayerController : MonoBehaviour
 
 
 	MOVE_STATE Move_State= MOVE_STATE.IDLE;
-	enum MOVE_STATE			{ IDLE,		WALK,	SPRINT,		CROUCH,		PORT_WALL_SLIDING,	STAR_WALL_SLIDING,	FALL_UP,	FALL_DOWN	}
-	float[] move_speed =	{ 0f,		10f,	10f,		4f,			4.5f,				4.5f,				5f,			3f			};
+	enum MOVE_STATE			{ IDLE,		WALK,	SPRINT,		CROUCH,		PORT_WALL_SLIDING,	STAR_WALL_SLIDING,	FALL_UP,	FALL_DOWN	} //TODO: Seporate FALL_UP and FALL_DOWN into own enum;
+	float[] move_speed =	{ 0f,		5f,		10f,		4f,			4.5f,				4.5f,				5f,			3f			};
 	const float move_deadzone      =  .2f	; 
 	const int   stop_lerp_duration = 30	    ;
 		  int   stop_lerp_elapsed  = 0		;
@@ -52,8 +52,14 @@ public class PlayerController : MonoBehaviour
 
 
 	Rigidbody body;
-	public GameObject rotationBody; float targetAngle; bool doRotation = true; float cached_input_angle;
-	const float max_rotate_degree = 5f, turn_deadzone = 40f, angle_difference_for_cam_snap = 35;
+	
+	public GameObject rotationBody; 
+	const float rotate_body_max = 3.5f;
+		  float targetAngle; 
+		  bool  rotate_toward_move_dir = true;
+	
+	float cached_input_angle;
+	const float turn_deadzone = 40f, angle_difference_for_cam_snap = 35;
 	
 	
 
@@ -75,6 +81,7 @@ public class PlayerController : MonoBehaviour
 
 		applyTransforms();
 
+		
 	}
 
 	#region Simple States which don't need to consider inputs
@@ -101,6 +108,44 @@ public class PlayerController : MonoBehaviour
 		}
 	
 	WALL_STATE determineWallState(bool drawCast=false ){
+	void applyGroundStateConsequences() {
+		switch (Ground_State) {
+			case GROUND_STATE.AIR:
+				body.useGravity = true;
+				break;
+
+			case GROUND_STATE.FLAT:
+				body.useGravity = false;
+				SnapToGround(ground);
+				break;
+
+			case GROUND_STATE.GENTLE:
+				body.useGravity = false;
+				break;
+
+			case GROUND_STATE.STEEP:
+				//TODO define Steep Slope Behavior.
+				break;
+			default: break;
+		}
+	}
+
+		void SnapToGround (RaycastHit ground) {
+			if (ground.distance < ground_snap_min) return;
+
+			float snapDist = ground.distance - ground_snap_offset;
+
+			Vector3 snapPos = body.position;
+			snapPos.y -= snapDist;
+			body.position = snapPos;
+
+			Vector3 linV = body.linearVelocity;
+			linV.y = Mathf.Max(0f, linV.y);
+			body.linearVelocity = linV;
+		}
+
+
+		WALL_STATE determineWallState(bool drawCast=false ){
 		lookingAtWFC = SharedLib.castWFC(body.position, rotationBody.transform.eulerAngles.y, check_wall_deg, check_wall_dist, drawCast);
 
 		//Check which rays found objects then use binary addition to determine wallState.
@@ -166,7 +211,7 @@ public class PlayerController : MonoBehaviour
 	MOVE_STATE handleMovementModifierInput() {return MOVE_STATE.IDLE;} //TODO: Impliment sprint/crouch keys if desired.
 
 	void panCameraModern(Vector2 panDir) { 
-		float input_angle = SharedLib.vectorToFlatAngle(panDir);
+		float input_angle = SharedLib.angleBetweenVectors(panDir,Vector2.right);
 		if (active_cam == (CinemachineCamera)cam_brain.ActiveVirtualCamera)						cached_input_angle = input_angle;
 		if ( Mathf.Abs(cached_input_angle - input_angle) >= angle_difference_for_cam_snap)		active_cam = (CinemachineCamera)cam_brain.ActiveVirtualCamera;	
 	}
@@ -187,19 +232,24 @@ public class PlayerController : MonoBehaviour
 	}
 	
 	void applyRotation() {
-		targetAngle-=.1f;
-		rotationBody.transform.rotation = Quaternion.AngleAxis(targetAngle,Vector3.down);
+		if (rotate_toward_move_dir) targetAngle= Quaternion.LookRotation( new Vector3(movedir2.x, 0, movedir2.y)).eulerAngles.y; 
+
+		Quaternion qFrom = rotationBody.transform.rotation;
+		Quaternion qToward = Quaternion.AngleAxis(targetAngle, Vector3.up);
+
+		rotationBody.transform.rotation = Quaternion.RotateTowards(qFrom,qToward,rotate_body_max);;
 	}
 
 	void applyMovement() {
-		movedir2            = respectWalls();
-		movedir3.x= movedir2.x * move_speed[(int)Move_State];
-		movedir3.z= movedir2.y * move_speed[(int)Move_State];
+		Vector2 respectfulMovedir2            = respectWalls().normalized;
+		respectfulMovedir2= (respectfulMovedir2 * move_speed[(int)Move_State]);
+		movedir3.x= respectfulMovedir2.x;
+		movedir3.z= respectfulMovedir2.y;
 
 		body.linearVelocity = movedir3;
 
 	}
-
+	
 	void stopMovement() {
 		Vector3 linV = body.linearVelocity;
 		linV.x = 0f;
@@ -221,7 +271,7 @@ public class PlayerController : MonoBehaviour
 		//body.linearVelocity = lerpV;
 	}
 
-	 Vector3 respectWalls() {
+	Vector3 respectWalls() {
 		Vector3[] dir3WFC= SharedLib.generateWFC(body.position, rotationBody.transform.eulerAngles.y, check_wall_deg);
 		Vector3 projectdir3= movedir3;
 		switch (Wall_State) {
@@ -239,104 +289,32 @@ public class PlayerController : MonoBehaviour
 	}
 
 
-	
-	
 
 
 
+	void OnTriggerEnter(Collider other) {
+		CameraSwitchTrigger cwt = other.GetComponent<CameraSwitchTrigger>();
+		if (cwt != null) {
+			cam_to_turnoff.gameObject.SetActive(false);
+			cwt.cam.gameObject.SetActive(true);
+			cam_to_turnoff = cwt.cam;
+
+			if (cwt.trackTarget)
+				cwt.cam.Target.TrackingTarget = camera_tracking_point.transform;
+			if (cwt.lookAtTarget)
+				cwt.cam.LookAt = camera_tracking_point.transform;
+		}
 
 
 
-
-
-
-//			body.linearVelocity = movedir3 * move_speed + Vector3.up * body.linearVelocity.y;
-
-//		}
-
-//		if (!moving) StopMoving();
-
-//		
-//		switch (groundState)
-//		{
-//			case GROUND_STATE.AIR:
-//				body.useGravity = true;
-//				break;
-
-//			case GROUND_STATE.FLAT:
-//				body.useGravity = false;
-//				SnapToGround(ground);
-//				break;
-
-//			case GROUND_STATE.GENTLE:
-//				body.useGravity = false;
-//				break;
-
-//			case GROUND_STATE.STEEP:
-//				//TODO define Steep Slope Behavior.
-//				break;
-//			default: break;
-//		}
-
-//	}
-
-//	//Takes a plane's normal and returns the plane's Grade(angle) in degrees.
-
-
-//	// Return whether a distance is greater than the altitude of a right triangle with this angle.whether the 
-
-
-//	void SnapToGround (RaycastHit ground)
-//	{
-//		if (ground.distance < ground_snap_min) return;
-
-//		float snapDist = ground.distance - ground_snap_offset;
-
-//		Vector3 snapPos = body.position;
-//		snapPos.y -= snapDist;
-//		body.position = snapPos;
-
-//		Vector3 linV = body.linearVelocity;
-//		linV.y = Mathf.Max(0f, linV.y);
-//		body.linearVelocity = linV;
-
-//	}
-
-
-//void FixedUpdate() {
-//	if (doRotation) {
-//		Debug.Log("target angle: " + targetAngle);
-//		Quaternion quatStart = rotationBody.transform.rotation;
-//		Quaternion quatTarget = Quaternion.LookRotation(SharedLib.angleToVector3(Mathf.Deg2Rad*targetAngle));
-//		Quaternion quatNew = Quaternion.RotateTowards(quatStart, quatTarget, max_rotate_degree);
-//		rotationBody.transform.rotation = quatNew;
-//	}
-//}
-
+	}
 }
 
-//	void OnTriggerEnter(Collider other)
-//	{
-//		CameraSwitchTrigger cwt = other.GetComponent<CameraSwitchTrigger>();
-//		if(cwt != null)
-//		{
-//			cam_to_turnoff.gameObject.SetActive(false);
-//			cwt.cam.gameObject.SetActive(true);
-//			cam_to_turnoff = cwt.cam;
-
-//			if(cwt.trackTarget)
-//				cwt.cam.Target.TrackingTarget = camera_tracking_point.transform;
-//			if(cwt.lookAtTarget)
-//				cwt.cam.LookAt = camera_tracking_point.transform;
-//		}
-//	}
 
 
-//	Vector2 skewByCamera(Vector2 inputs)
-//	{
-//		float camAngle = 360f - active_cam.transform.eulerAngles.y; //eulerAngles.y in typical math represent pitch, but because Unity:tm:, eulerAngles.y is acting as yaw.
-//		return SharedLib.rotateVector2(camAngle, inputs);
-//	}
-//	void updateTargetAngle(Vector2 dir) { targetAngle = SharedLib.angleBetweenVectors(Vector2.right, dir); }
 
-//}
+
+
+
+
+
