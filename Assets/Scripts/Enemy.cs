7c71e459834cd401a0b81e8be392900f4d1f7d27
AnDrew <repooc.a@gmail.com>
2026-08-07 23:19:23 -0500

    Seporated Movement from Enemy into Enemy Spinner

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,24 +2,21 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    Rigidbody body;
-    const float force_multiplier	=	 8		, 
-				motion_drag			=	 4.5f	,
-                move_speed          =    0.8f   ;
+    public Rigidbody body;
+    public GameObject rotationBody;
 
     RaycastHit[] scanSweep;
     const float half_fov=90, scan_distance=50;
     const int scan_ray_density=10;
 
-    bool playerSeen;
-    GameObject playerInSight;
-    Vector3 lastKnownPlayerLocation= new Vector3(1000,1000,1000);
-    const float find_player_distance = 20,
-                track_player_distance= 10;
+    public bool playerIsInSight;
+    GameObject PlayerSeen { get; set; }
+    public Vector3 lastKnownPlayerLocation= new Vector3(1000,1000,1000); // The default vector is this as a hacky way of making sure the enemy doesn't start pathfinding to the default LKPL immediately
+    public const float find_player_distance = 20,
+                       track_player_distance= 10;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    void Start() {
         getComponentFields();
     }
 
@@ -30,8 +27,8 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update() {
         scanEnvironment();
-        findPlayer();
-        decideMovement();
+        lookForPlayer();
+        inheritorSpecificUpdate();
     }
 
     void scanEnvironment() {
@@ -54,43 +51,24 @@ public class Enemy : MonoBehaviour
 
     }
 
-    void findPlayer() {
-        playerSeen=false;
+    void lookForPlayer() {
+        playerIsInSight=false;
         //Debug.Log("Start Scan");
 
         foreach (RaycastHit hit in scanSweep){
             if (hit.collider!=null && 1<<hit.transform.gameObject.layer==LayerMask.GetMask("Player") && hit.distance<find_player_distance) {
-                playerSeen=true; 
-                playerInSight=hit.transform.gameObject;    
+                playerIsInSight=true; 
+                PlayerSeen=hit.transform.gameObject;    
             }   
         }
-        if (playerSeen) {
+        if (playerIsInSight) {
             Rigidbody playerBody;
-            playerInSight.TryGetComponent<Rigidbody>(out playerBody);
+            PlayerSeen.TryGetComponent<Rigidbody>(out playerBody);
             lastKnownPlayerLocation= playerBody.position;
         }
-        else { playerInSight=null;}
+        else { PlayerSeen=null;}
     }
 
-	void decideMovement() { 
-        if(playerSeen) { 
-            MoveTowardPlayer();
-        }
-        else {
-            Wander();
-        }
-	}
-
-    void MoveTowardPlayer() {
-        body.AddForce(  (lastKnownPlayerLocation-body.position)*force_multiplier, ForceMode.Force  );
-        body.linearDamping=motion_drag;
-        
-        if (body.linearVelocity.magnitude > move_speed) body.linearVelocity= body.linearVelocity.normalized*move_speed;
-    }
-
-    void Wander() {
-
-    }
-
+    virtual public void inheritorSpecificUpdate() { }
 
 }
--- Assets/Scripts/EnemySpinner.cs
+++ Assets/Scripts/EnemySpinner.cs
@@ -3,6 +3,49 @@ using UnityEngine;
 public class EnemySpinner : Enemy
 {
 
+    const float force_multiplier	=	 8		, 
+		        motion_drag 		=	 2.5f	,
+                move_speed          =    0.8f   ;
+    Vector3 AxisOfRotation= new Vector3 (-36.0f, 0, -20.7f);
+    float AngleofRotation=0;
+	
+
+
+
+	override public void inheritorSpecificUpdate() {
+        decideMovement();
+    }
+
+     void decideMovement() { 
+        if(playerIsInSight) { 
+            MoveTowardPlayer();
+        }
+        Wander();
+        
+	}
+
+    void MoveTowardPlayer() {
+        body.AddForce(  (lastKnownPlayerLocation-body.position)*force_multiplier, ForceMode.Force  );
+        body.linearDamping=motion_drag;
+        
+        if (body.linearVelocity.magnitude > move_speed) body.linearVelocity= body.linearVelocity.normalized*move_speed;
+    }
+
+    void Wander() {
+        Vector3 randVec= new Vector3 (Random.Range(-.1f, 0.3f),Random.Range(-.1f, .3f),Random.Range(-.1f,.3f) );
+        Vector3 rbr= rotationBody.transform.rotation.eulerAngles;
+        Vector3 sum= rbr+randVec;
+
+        Quaternion randDelta = Quaternion.Euler(sum.x,sum.y, sum.z);
+
+        //The little ball isn't rotating the way I want it to, ~~but it is rotating~~.
+        rotationBody.transform.rotation= randDelta;
+    }
+
+
+
+
+
 
 
 }
