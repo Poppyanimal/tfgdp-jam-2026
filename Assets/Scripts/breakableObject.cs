82fc37d25e07ceb56a91e560768348cad3b5e285
Aubrey <Poppyanimal@Yahoo.com>
2026-08-11 17:11:37 -0500

    breakable box

---
+++ Assets/Scripts/breakableObject.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class breakableObject : MonoBehaviour
+{
+    public GameObject modelToHide;
+
+    Rigidbody body; Collider col;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == 9)
+        {
+            body.isKinematic = true;
+            col.enabled = false;
+            modelToHide.SetActive(false);
+            ParticleSystem[] parts = GetComponentsInChildren<ParticleSystem>();
+            foreach(ParticleSystem part in parts)
+            {
+                part.Play();
+            }
+        }
+    }
+}
